64358c1c20fc2a85d90a2fe133a173e58a980544
Disteonne <diana.razmikovna@yandex.ru>
2020-12-21 00:56:39 +0300

    lesson 2

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -4,6 +4,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpring {
     public static void main(String[] args) {
+        /*         LESSON 1: TestBean
         ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
@@ -11,5 +12,22 @@ public class TestSpring {
         System.out.println(testBean.getName());
 
         context.close();
+         */
+
+        ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+
+        //          ДО ВНЕДРЕНИЯ ЗАВИСИМОСТЕЙ
+        //Music music=context.getBean("musicBean",Music.class);
+        //MusicPlayer player=new MusicPlayer(music);
+        //player.playMusic();
+
+        MusicPlayer musicPlayer=context.getBean("musicPlayer",MusicPlayer.class);
+        musicPlayer.playMusic();
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
+        context.close();
+
     }
 }
